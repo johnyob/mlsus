c5b000a984fef7273f0b4707f240b1b5faf6d50c
johnyob <ajo41@cam.ac.uk>
2025-01-03 19:01:07 +0000

    fix(type_checker): correctly handle mutually recursive types and check arities

--- lib/type_checker/mlsus_type_checker.ml
+++ lib/type_checker/mlsus_type_checker.ml
@@ -1,13 +1,27 @@
 open! Import
 open Constraint
 
-let infer_exp exp =
+let infer_exp_with_env_transformer ?(env_xfmr = Fn.id) exp =
   Predef.Env.wrap
   @@ fun env ->
   let open Or_error.Let_syntax in
-  let exp_type = Type.Var.create ~id_source:env.id_source ~name:"exp_type0" () in
-  let%map c = Infer.Expression.infer_exp ~env exp (Type.var exp_type) in
+  let exp_type = Type.Var.create ~id_source:(Env.id_source env) ~name:"exp_type0" () in
+  let%map c = Infer.Expression.infer_exp ~env:(env_xfmr env) exp (Type.var exp_type) in
   exists exp_type c
 ;;
 
-let infer_str str = Predef.Env.wrap @@ fun env -> Infer.Structure.infer_str ~env str
+let infer_str_with_env_transformer ?(env_xfmr = Fn.id) str =
+  Predef.Env.wrap @@ fun env -> Infer.Structure.infer_str ~env:(env_xfmr env) str
+;;
+
+let infer_exp exp = infer_exp_with_env_transformer exp
+let infer_str str = infer_str_with_env_transformer str
+
+module Private = struct
+  module Env = Env
+  module Adt = Adt
+  module Predef = Predef
+
+  let infer_exp = infer_exp_with_env_transformer
+  let infer_str = infer_str_with_env_transformer
+end
