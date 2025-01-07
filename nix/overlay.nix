7961b870129fbe84303519b3738ca436f0a4a319
johnyob <ajo41@cam.ac.uk>
2025-01-07 21:19:13 +0000

    feat(ast): add source ranges to AST

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -14,9 +14,11 @@ with prev; {
 
               minimalOCamlVersion = "4.14";
 
-              src = fetchurl {
-                url = "https://github.com/johnyob/grace/releases/download/${version}/grace-${version}.tbz";
-                hash = "sha256-gh31SILJJT6safR7zzpx/9xhx3/a5CWHwyqtpbVs/q4=";
+              src = fetchFromGitHub {
+                owner = "johnyob";
+                repo = "grace";
+                rev = "275adda398834612b804283fe9548708e570bfdf";
+                hash = "sha256-2zBYXrNb/rVWjEBmmQfW+rAWs/qhx6nMhAdJEwIdhHQ=";
               };
               propagatedBuildInputs = [core ppx_jane fmt dedent iter core_unix uutf ppx_optcomp];
             };
