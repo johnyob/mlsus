178281ae0a1a9491ea703b3acbe44283d0ce9662
johnyob <ajo41@cam.ac.uk>
2025-03-24 17:21:10 +0000

    feat(source): add `Mlsus_source` to register current source
    The `Mlsus_source` library will track the current source. This
    is useful in lexers / parsers where we need access to the
    current source (to create ranges)

---
+++ lib/source/mlsus_source.ml
@@ -0,0 +1,58 @@
+open Core
+open Grace
+
+let state : Source.t option ref = ref None
+let get () = !state
+
+let get_exn () =
+  match !state with
+  | Some src -> src
+  | None -> raise_s [%message "State is not initialized"]
+;;
+
+let assert_state_is_empty () =
+  if Option.is_some !state
+  then raise_s [%message "State is already initialized" (!state : Source.t option)]
+;;
+
+let init_optional src =
+  assert_state_is_empty ();
+  state := src
+;;
+
+let init src =
+  assert_state_is_empty ();
+  state := Some src
+;;
+
+let clear () = state := None
+
+let with_source ~source f =
+  Fun.protect
+    (fun () ->
+       init source;
+       f ())
+    ~finally:clear
+;;
+
+let with_optional_source ?source f =
+  Fun.protect
+    (fun () ->
+       init_optional source;
+       f ())
+    ~finally:clear
+;;
+
+module For_testing = struct
+  let expect_test_source str : Source.t =
+    (* Grace's `string_source` has a frustrating bug in `sexp_of_string_source`
+       which prints the contents of string source out. This results in a huge expect
+       test output. A quick fix is to construct a string source manually (using a reader source) *)
+    `Reader
+      { id = 0
+      ; name = Some "expect_test.ml"
+      ; length = String.length str
+      ; unsafe_get = String.unsafe_get str
+      }
+  ;;
+end
---
+++ lib/source/mlsus_source.mli
@@ -0,0 +1,19 @@
+open Grace
+
+(** [with_source src f] runs [f] with the current source set to [src],
+    clearing it once [f] returns. *)
+val with_source : source:Source.t -> (unit -> 'a) -> 'a
+
+(** [with_optional_source] optionally sets the source and runs [f]. *)
+val with_optional_source : ?source:Source.t -> (unit -> 'a) -> 'a
+
+(** [get ()] returns the current source.
+    Returns [None] if no source is set. *)
+val get : unit -> Source.t option
+
+(** [get_exn ()] returns the current source. *)
+val get_exn : unit -> Source.t
+
+module For_testing : sig
+  val expect_test_source : string -> Source.t
+end
